5bb574ba7dfa375fdf50ce778998b823314b5651
nc6d <6od92002@gmail.com>
2020-09-21 20:00:32 +0300

    commit1

---
+++ Task_2/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter c: ");
+            long c = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine($" Number A: {a}\n Number B: {b}\n Number C: {c}");
+        }
+    }
+}
